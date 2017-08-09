c1178a2d555995f30638ee82927e105c18a9ccac
ice <ice@lab06m28.nrc.ice.ufjf.br>
2017-08-08 22:40:35 -0300

    Término do exercício.

--- src/aula01exercicio00/Aula01Exercicio00.java
+++ src/aula01exercicio00/Aula01Exercicio00.java
@@ -2,19 +2,22 @@ package aula01exercicio00;
 
 import javax.swing.JOptionPane;
 
-
 public class Aula01Exercicio00 {
 
-
     public static void main(String[] args) {
-      String largura;
+        String largura;
         String comprimento;
         String alturaCm;
 
         largura = JOptionPane.showInputDialog("Digite a largura:");
         comprimento = JOptionPane.showInputDialog("Digite o comprimento:");
         alturaCm = JOptionPane.showInputDialog("Digite a altura em cm:");
-        
+
+        Double la = Double.parseDouble(largura);
+        Double com = Double.parseDouble(comprimento);
+        Double al = Double.parseDouble(alturaCm);
+
+        JOptionPane.showMessageDialog(null, "O volume é: " + la * com * al, "Resultado", JOptionPane.PLAIN_MESSAGE);
     }
-    
+
 }
