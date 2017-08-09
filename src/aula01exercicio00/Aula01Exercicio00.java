00cff5694e64772726fcd1c8bcdbd8907f09d106
ice <ice@lab06m28.nrc.ice.ufjf.br>
2017-08-08 22:39:02 -0300

    Adição de variáveis

--- src/aula01exercicio00/Aula01Exercicio00.java
+++ src/aula01exercicio00/Aula01Exercicio00.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula01exercicio00;
 
-/**
- *
- * @author ice
- */
+import javax.swing.JOptionPane;
+
+
 public class Aula01Exercicio00 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+      String largura;
+        String comprimento;
+        String alturaCm;
+
+        largura = JOptionPane.showInputDialog("Digite a largura:");
+        comprimento = JOptionPane.showInputDialog("Digite o comprimento:");
+        alturaCm = JOptionPane.showInputDialog("Digite a altura em cm:");
+        
     }
     
 }
